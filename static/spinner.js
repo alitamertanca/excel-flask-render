feb8ad3194d47277ba45f323c42d7cce19121e60
Ali Tamer Tanca <aizmir@amertanca.com.tr>
2025-05-29 15:58:06 +0300

    Avantajlı Fiyat Hesaplama v0.1

--- static/spinner.js
+++ static/spinner.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
+    const form = document.getElementById("form");
     const spinner = document.getElementById("spinner");
     const submitBtn = document.getElementById("submitBtn");
 
     form.addEventListener("submit", function () {
         spinner.style.display = "block";
         submitBtn.disabled = true;
+
+        // Spinner'ı 10 saniye sonra otomatik gizle
+        setTimeout(() => {
+            spinner.style.display = "none";
+            submitBtn.disabled = false;
+        }, 10000); // işlem ortalama süresine göre ayarlanabilir
     });
 });
